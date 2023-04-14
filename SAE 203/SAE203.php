6f2a7786c01ac613183a7a2343578013ebd0976d
JSo77 <130764918+JSo77@users.noreply.github.com>
2023-04-14 17:50:10 +0200

    Add files via upload

--- SAE 203/SAE203.php
+++ SAE 203/SAE203.php
@@ -1,8 +1,20 @@
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-<META charset="utf-8">
-<link rel="stylesheet" href="Styles/SAE203.css"> 
-<script src=""></script>    
-</head>
-<body>
+<html>
+  <head>
+    <title>Connexion</title>
+    <link rel="stylesheet" href="Styles/SAE203.css">
+  </head>
+  <body>
+    <div class="container">
+      <form>
+        <h2>Connectez-vous !</h2>
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" id="username" name="username" placeholder="Entrez votre nom d'utilisateur" required>
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe" required>
+        <button type="submit">Se connecter</button>
+        <p>Vous n'avez pas de compte ? Inscrivez vous <a href="">ici</a> !</p>
+      </form>
+    </div>
+  </body>
+</html>
