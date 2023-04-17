fa591cf11972b96e3d7edb8472fd01d393228e4e
JSo77 <130764918+JSo77@users.noreply.github.com>
2023-04-17 15:36:59 +0200

    Add files via upload

--- SAE 203/SAE203.php
+++ SAE 203/SAE203.php
@@ -13,7 +13,8 @@
         <label for="password">Mot de passe :</label>
         <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe" required>
         <button type="submit">Se connecter</button>
-        <p class="p_c">Vous n'avez pas de compte ?<a href=""> Inscrivez vous ici !</a></p>
+        <p class="p_c">Vous n'avez pas de compte ? <a href="
+http://localhost/index.html/SAE%20203/i_SAE203.php?username=eeee&nom=eeee&prenom=eeee&email=eeee%40gmail.com&datenaiss=5555-04-25&password=ggggg"> Inscrivez vous ici !</a></p>
       </form>
     </div>
   </body>
--- SAE 203/i_SAE203.php
+++ SAE 203/i_SAE203.php
@@ -2,18 +2,30 @@
 <html>
   <head>
     <title>Inscription</title>
-    <link rel="stylesheet" type="text/css" href="Styles/SAE203.css"> 
+    <link rel="stylesheet" type="text/css" href="Styles/i_SAE203.css"> 
   </head>
   <body>
     <div class="container">
       <form>
         <h1>Inscrivez-vous !</h1>
-        <label for="username">Nom d'utilisateur :</label>
+        
+        <label for="pseudo">Nom d'utilisateur :</label>
         <input type="text" id="username" name="username" placeholder="Entrez votre nom d'utilisateur" required>
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="nom" placeholder="Entrez votre nom" required>
+        <label for="prenom">Prénom :</label>
+        <input type="text" id="prenom" name="prenom" placeholder="Entrez votre prénom" required>
+        
+        
+        <label for="email">Adresse e-mail :</label>
+        <input type="email" id="email" name="email" placeholder="Entrez une adresse e-mail valide" required>
+        <label for="datenaiss">Date de naissance :</label>
+        <input type="date" id="datenaiss" name="datenaiss" placeholder="" required>
         <label for="password">Mot de passe :</label>
         <input type="password" id="password" name="password" placeholder="Entrez votre mot de passe" required>
-        <button type="submit">Se connecter</button>
-        <p class="p_c">Vous avez déjà un compte ?<a href=""> Connectez-vous ici !</a></p>
+        
+        <button type="submit">S'inscrire</button>
+        <p class="p_c">Vous avez déjà un compte ? <a href="http://localhost/index.html/SAE%20203/SAE203.php"> Connectez-vous ici !</a></p>
       </form>
     </div>
   </body>
