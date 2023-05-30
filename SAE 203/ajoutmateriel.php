d59634e05f7d0eab6142867573eccc62c7c4d587
Ales230 <130764921+Ales230@users.noreply.github.com>
2023-05-30 23:00:43 +0200

    Add files via upload

--- SAE 203/ajoutmateriel.php
+++ SAE 203/ajoutmateriel.php
@@ -50,7 +50,7 @@ function afficherRoleNavigation($role)
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="Styles/ajoutmateriel.css" />
+  <link rel="stylesheet" href="styles/ajoutmateriel.css" />
 
   <title>Ajout de nouveau matériel</title>
 </head>
--- SAE 203/reservation.php
+++ SAE 203/reservation.php
@@ -101,7 +101,7 @@ if ($role === '2') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/reservation.css" />
+    <link rel="stylesheet" href="Styles/reservation.css" />
     <title>Demande de réservation</title>
 </head>
 <body>
